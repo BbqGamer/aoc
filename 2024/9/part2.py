a40345cff957423e09ca585f9465c8c84a6e650f
BbqGamer <korba.adam@gmail.com>
2024-12-09 20:48:07 +0100

    wip

--- 2024/9/part2.py
+++ 2024/9/part2.py
@@ -1,3 +1,4 @@
+import heapq
 from itertools import accumulate
 
 disk = list(map(int, input()))
@@ -7,28 +8,43 @@ D = list(zip(disk, offsets))
 
 full = D[::2]
 empty = D[1::2]
+index = []
+for i in range(10):
+    index.append([])
+
+for n, eoffset in empty:
+    heapq.heappush(index[n], eoffset)
+
+string = ["."] * 65000
 
 checksum = 0
 for identifier in reversed(range(len(full))):
     moved = False
     n, offset = full[identifier]
-    for i in range(len(empty)):
-        en, eoffset = empty.pop(i)
-        if offset <= eoffset:
-            break
-
-        if n > en:
-            empty.insert(i, (en, eoffset))
+    for en in range(n, len(index)):
+        if not index[en]:
             continue
+
+        eoffset = heapq.heappop(index[en])
+        if offset < eoffset:
+            heapq.heappush(index[en], eoffset)
+            continue
+
+        for x in range(eoffset, eoffset + n):
+            string[x] = str(identifier)
 
         checksum += ((n * (n - 1)) // 2 + eoffset * n) * identifier
         diff = en - n
         if diff:
-            empty.insert(i, (diff, eoffset + n))
+            heapq.heappush(index[diff], eoffset + n)
         moved = True
         break
     if not moved:
+        for x in range(offset, offset + n):
+            string[x] = str(identifier)
         checksum += ((n * (n - 1)) // 2 + offset * n) * identifier
 
+
+print("".join(string))
 
 print(checksum)
