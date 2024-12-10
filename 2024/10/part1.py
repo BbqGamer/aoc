5947732ca7cacccd0d8ea15a32f80b8f10922f75
BbqGamer <korba.adam@gmail.com>
2024-12-10 10:04:50 +0100

    Finish part 1 day 10

---
+++ 2024/10/part1.py
@@ -0,0 +1,32 @@
+import fileinput
+
+grid = [list(map(int, line.strip())) for line in fileinput.input()]
+
+trailheads = []
+for y, row in enumerate(grid):
+    for x, h in enumerate(row):
+        if h == 0:
+            trailheads.append((y, x))
+
+count = 0
+for starty, startx in trailheads:
+    reachable = set()
+    stack = [(starty, startx)]
+    while stack:
+        y, x = stack.pop()
+        h = grid[y][x]
+        if h == 9:
+            reachable.add((y, x))
+            continue
+
+        if x > 0 and grid[y][x - 1] - h == 1:
+            stack.append((y, x - 1))
+        if y > 0 and grid[y - 1][x] - h == 1:
+            stack.append((y - 1, x))
+        if x < len(grid) - 1 and grid[y][x + 1] - h == 1:
+            stack.append((y, x + 1))
+        if y < len(grid[0]) - 1 and grid[y + 1][x] - h == 1:
+            stack.append((y + 1, x))
+    count += len(reachable)
+
+print(count)
