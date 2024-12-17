05c471a1deae58086c91831f98626664b33f2a95
BbqGamer <korba.adam@gmail.com>
2024-12-17 16:17:37 +0100

    Finish part 1

--- 2024/17/part1.py
+++ 2024/17/part1.py
@@ -1,14 +1,60 @@
 import re
 import sys
-from typing import NamedTuple
+from dataclasses import dataclass
 
 
-class State(NamedTuple):
-    reg_a: int
-    reg_b: int
-    reg_c: int
+@dataclass
+class State:
+    reg_A: int
+    reg_B: int
+    reg_C: int
     program: list[int]  # 3-bit numbers
     ip: int = 0
+
+    output = ""
+
+    def step(self) -> bool:
+        if self.ip >= len(self.program):
+            return True
+
+        opcode, operand = self.program[self.ip : self.ip + 2]
+        match opcode:
+            case 0:  # adv
+                self.reg_A = int(self.reg_A / 2 ** self.combo(operand))
+            case 1:  # bxl
+                self.reg_B = self.reg_B ^ operand
+            case 2:  # bst
+                self.reg_B = self.combo(operand) % 8
+            case 3:  # jnz
+                if self.reg_A != 0:
+                    self.ip = operand
+                    return False
+            case 4:  # bxc
+                self.reg_B = self.reg_B ^ self.reg_C
+            case 5:  # out
+                if self.output:
+                    self.output += ","
+                self.output += str(self.combo(operand) % 8)
+            case 6:  # bdv
+                self.reg_B = int(self.reg_A / self.combo(operand))
+            case 7:  # cdv
+                self.reg_C = int(self.reg_A / self.combo(operand))
+            case _:
+                print("Unknown opcode")
+        self.ip += 2
+        return False
+
+    def combo(self, operand: int) -> int:
+        if operand >= 0 and operand <= 3:
+            return operand
+        elif operand == 4:
+            return self.reg_A
+        elif operand == 5:
+            return self.reg_B
+        elif operand == 6:
+            return self.reg_C
+        else:
+            raise ValueError("Invalid operand")
 
 
 def parse(data: str) -> State:
@@ -18,9 +64,14 @@ def parse(data: str) -> State:
     m = re.match(PATTERN, data)
     if m is None:
         raise ValueError("Invalid input")
-    reg_a, reg_b, reg_c, program = m.groups()
-    return State(int(reg_a), int(reg_b), int(reg_c), list(map(int, program.split(","))))
+    reg_A, reg_B, reg_C, program = m.groups()
+    return State(int(reg_A), int(reg_B), int(reg_C), list(map(int, program.split(","))))
 
 
 if __name__ == "__main__":
-    print(parse(sys.stdin.read()))
+    state = parse(sys.stdin.read())
+
+    while not state.step():
+        pass
+
+    print(state.output)
