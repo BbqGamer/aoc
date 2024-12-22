d15c4da41ef4881b9f69fd221a02b6cbec76f1f4
BbqGamer <korba.adam@gmail.com>
2024-12-22 16:24:42 +0100

    Complete part 2 of 22 day AoC

--- 2024/22/solution.py
+++ 2024/22/solution.py
@@ -1,8 +1,12 @@
 import sys
+from collections import defaultdict
 
-res = 0
+part1 = 0
+quaternions_profit_sum = defaultdict(int)
+
 for line in sys.stdin.readlines():
     num = int(line)
+    sequence = [num % 10]
     for _ in range(2000):
         num = num ^ (num << 6)
         num %= 16777216
@@ -13,5 +17,23 @@ for line in sys.stdin.readlines():
         num = num ^ (num << 11)
         num %= 16777216
 
-    res += num
-print(res)
+        sequence.append(num % 10)
+    part1 += num
+
+    diffs = []
+    for i in range(len(sequence) - 1):
+        diffs.append(sequence[i + 1] - sequence[i])
+    sequence.pop(0)
+
+    profits = defaultdict(int)
+    for i in range(3, len(diffs)):
+        quaternion = tuple(diffs[i - 3 : i + 1])
+        profit = sequence[i]
+        if quaternion not in profits:
+            profits[quaternion] = profit
+
+    for quaternion, profit in profits.items():
+        quaternions_profit_sum[quaternion] += profit
+
+print("Part 1: ", part1)
+print("Part 2: ", max(quaternions_profit_sum.values()))
