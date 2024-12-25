0808f248aaf603d278dfbdea1d311782fe8832b6
BbqGamer <korba.adam@gmail.com>
2024-12-25 12:00:20 +0100

    Finish part 1 of day 25

--- 2024/25/part1.py
+++ 2024/25/part1.py
@@ -1,13 +1,17 @@
+import itertools
 import sys
 
 data = sys.stdin.read().strip().split("\n\n")
 
-keys = [], locks = []
+WIDTH = 5
+HEIGHT = 7
+
+keys, locks = [], []
 for d in data:
     arr = d.split("\n")
-    counts = [0 for _ in range(5)]
-    for i in range(7):
-        for j in range(5):
+    counts = [0 for _ in range(WIDTH)]
+    for i in range(1, HEIGHT - 1):
+        for j in range(WIDTH):
             if arr[i][j] == "#":
                 counts[j] += 1
 
@@ -15,3 +19,13 @@ for d in data:
         locks.append(tuple(counts))
     else:
         keys.append(tuple(counts))
+
+
+result = 0
+for key, lock in itertools.product(keys, locks):
+    sumed = [key[i] + lock[i] for i in range(WIDTH)]
+    if all(s <= HEIGHT - 2 for s in sumed):
+        result += 1
+        continue
+
+print(result)
