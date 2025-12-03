3873b839917f4a91f99c8f6609f89c4184c11452
BbqGamer <korba.adam@gmail.com>
2025-12-03 18:34:54 +0100

    2025 day 1 part 2 in Haskell

---
+++ 2025/1/part2.hs
@@ -0,0 +1,27 @@
+parse :: String -> Int
+parse ('L':xs) = - (read xs :: Int)
+parse ('R':xs) =   read xs :: Int
+
+hits :: Int -> Int -> (Int, Int)
+hits s m
+    | m > 0  = (mod (s + m) 100, div (s + m) 100)
+    | m < 0  = (mod (s + m) 100, div ((if s == 0 then 0 else 100 - s) - m) 100)
+    | otherwise = (s, 0)
+    
+
+applyMoves :: [Int] -> (Int, Int)
+applyMoves = foldl step (50, 0)
+    where
+        step :: (Int, Int) -> Int -> (Int, Int)
+        step (state, total) move =
+            let (newState, val) = hits state move
+                newTotal = total + val
+            in (newState, newTotal)
+
+main :: IO ()
+main = do
+    contents <- getContents
+    let inputLines = lines contents
+    let parsed = map parse inputLines
+    let (finalState, totalSum) = applyMoves parsed
+    print totalSum
