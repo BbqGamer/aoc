7759c77ee6d95a28a8fca6b462988ced883f6e65
BbqGamer <korba.adam@gmail.com>
2025-12-07 00:45:44 +0100

    2025 Day 4 refactor in haskell

--- 2025/4/parts.hs
+++ 2025/4/parts.hs
@@ -5,54 +5,44 @@ rolling k xs
     | length xs < k = []
     | otherwise     = take k xs : rolling k (tail xs)
 
-
 rollingGrid :: Int -> Int -> [[a]] -> [[[[a]]]]
-rollingGrid w h grid = 
-    let
-        horizontal = map (rolling w) grid
-        vertical = rolling h horizontal
-        rolled = map transpose vertical
-    in
-        rolled
+rollingGrid w h = map transpose . rolling h . map (rolling w)
 
 trans :: [[Char]] -> Char
-trans window = 
-    let 
-        flat = concat window
-    in 
-        if flat !! 4 /= '@' then '.' else if (count (=='@') flat) <= 4 then '.' else '@'
+trans window
+    | center /= '@'     = '.'
+    | rockCount <= 4    = '.'
+    | otherwise         = '@'
+  where
+    flat = concat window
+    center = flat !! 4
+    rockCount = length (filter (== '@') flat)
 
-    
 transform :: [[Char]] -> [[Char]]
-transform grid = map (map trans) (rollingGrid 3 3 (padGrid 2 '.' (grid)))
+transform = map (map trans) . rollingGrid 3 3 . padGrid 1 '.'
 
-    
 padGrid :: Int -> a -> [[a]] -> [[a]]
-padGrid k padVal grid =
-    let 
-        paddingSize = k `div` 2
-        padRow r = replicate paddingSize padVal ++ r ++ replicate paddingSize padVal
-        horizontallyPadded = map padRow grid
-        rowWidth  = length (head horizontallyPadded)
-        emptyRow  = replicate rowWidth padVal
-        verticalPad = replicate paddingSize emptyRow
-    in 
-        verticalPad ++ horizontallyPadded ++ verticalPad
-
-count :: (a -> Bool) -> [a] -> Int
-count p xs = length (filter p xs)
+padGrid padding padVal grid = verticalPad ++ map padRow grid ++ verticalPad
+  where
+    padRow = (replicate padding padVal ++) . (++ replicate padding padVal)
+    rowWidth = length (head grid) + 2 * padding
+    verticalPad = replicate padding (replicate rowWidth padVal)
 
 countRocks :: [[Char]] -> Int
-countRocks grid = sum (map (count (=='@')) grid)
+countRocks = length . filter (== '@') . concat
 
-repeatUntilStable grid = 
-    let new = transform grid
-    in if grid == new then grid else repeatUntilStable new
+repeatUntilStable :: [[Char]] -> [[Char]]
+repeatUntilStable grid
+    | grid == new = grid
+    | otherwise   = repeatUntilStable new
+  where
+    new = transform grid
 
 main :: IO ()
 main = do
     start <- lines <$> getContents
     let part1 = transform start
-    print ((countRocks start) - (countRocks part1))
-    let part2 = repeatUntilStable part1
-    print ((countRocks start) - (countRocks part2))
+        part2 = repeatUntilStable part1
+        startCount = countRocks start
+    print (startCount - countRocks part1)
+    print (startCount - countRocks part2)
