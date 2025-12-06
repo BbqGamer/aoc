e98bed663ed52f07a1dd151c38574314156c1e5a
BbqGamer <korba.adam@gmail.com>
2025-12-06 22:47:25 +0100

    2025 Day 6 refactor haskell code

--- 2025/6/parts.hs
+++ 2025/6/parts.hs
@@ -1,36 +1,24 @@
 import Data.List (transpose)
-import Data.Char (digitToInt)
 
 calc :: [String] -> Int
 calc ("+":xs) = sum (map read xs)
 calc ("*":xs) = product (map read xs)
 calc _ = 0
 
-noSpace s = filter (/=' ') s
-
--- convertCol col = map digitToInt col
-
-calc2 ("+", xs) = sum (map read xs)
-calc2 ("*", xs) = product (map read xs)
-
-parseLine [] str = []
-parseLine (l : lengths) str =
-    take l str : parseLine lengths (drop (l + 1) str)
-
+parseLine :: [Int] -> String -> [String]
+parseLine [] _ = []
+parseLine (l:lengths) str = take l str : parseLine lengths (drop (l + 1) str)
 
 main :: IO ()
 main = do
     input <- lines <$> getContents
-    let processed = (transpose . map words . reverse) input 
-    print $ sum (map calc processed)
+    let part1 = sum . map calc . transpose . map words . reverse $ input
+    print part1
 
-    let numbers = init input
-    let ops = words (last input)
-    let processed = (transpose . map words) numbers
-
-    let lengths = map (maximum . map length) processed
-    let parsed = map (parseLine lengths) numbers
-    let transposed = map transpose (transpose parsed)
-    let zipped = zip ops transposed
-    print zipped
-    print (sum (map calc2 zipped))
+    let values = init input
+        ops = words (last input)
+        columns = transpose (map words values)
+        lengths = map (maximum . map length) columns
+        parsed = map (parseLine lengths) values
+        transposed = map transpose (transpose parsed)
+    print $ sum (map (calc . uncurry (:)) (zip ops transposed))
