0cf4260e64617dc1656ca8b0ec7ef6b2a433b830
BbqGamer <korba.adam@gmail.com>
2025-12-07 13:14:00 +0100

    Refactor code in haskell

--- 2025/7/parts.hs
+++ 2025/7/parts.hs
@@ -1,33 +1,29 @@
-import qualified Data.Map as Map
-import qualified Data.Set as Set
+roll :: [a] -> [(a, a, a)]
+roll (x:y:z:xs) = (x, y, z) : roll (y:z:xs)
+roll _          = []
 
-merge (i, xs) =
-    map (\(a, b) -> ((i, a), b)) xs
+nState :: (Int, Int, Int) -> Int -> Int
+nState (l, 0, r) val = val + l + r
+nState _         _   = 0
 
-move elem = (\(a, b) -> (succ a, b))
+sim :: ([Int], [Char]) -> ([Int], Int)
+sim (state, row) = (zipWith nState windows state, count)
+    where
+        toSplit = map (\(v, c) -> if c == '^' then v else 0) (zip state row)
+        windows = roll (0 : toSplit ++ [0])
+        count = length (filter (> 0) toSplit)
 
-simulate splitters beams limN limM 
-    | Set.null beams = 0
-    | otherwise =
-        let 
-            moved = Set.map (\(a, b) -> (succ a, b)) beams
-            filtered = Set.filter (\(a, b) -> a >= 0 && b >= 0 && a <= limN && b <= limM) moved
-            toSplit = filtered `Set.intersection` splitters
-            noSplit = filtered `Set.difference` toSplit
-            splitted = Set.unions (Set.map (\(a, b) -> Set.fromList ([(a, b - 1), (a, b + 1)])) toSplit)
-            newbeams = splitted `Set.union` noSplit
-        in
-            simulate splitters newbeams limN limM + Set.size toSplit
+simulate :: [String] -> [Int] -> Int -> ([Int], Int)
+simulate [] state acc = (state, acc)
+simulate (row : rows) state acc = 
+    let (newState, cnt) = sim (state, row)
+    in simulate rows newState (acc + cnt)
+
+parseInput :: String -> [Int]
+parseInput = map (\c -> if c == '.' then 0 else 1)
 
 main = do
     input <- lines <$> getContents
-
-    let zipped = zip [0..] (map (zip [0..]) input)
-    let dict = Map.fromList (concat (map merge zipped))
-
-    let start = Map.keysSet (Map.filter (== 'S') dict)
-    let splitters = Map.keysSet (Map.filter (== '^') dict)
-
-    let (n, m) = maximum (Set.toList splitters)
-    let simulated = simulate splitters start n m
-    print simulated
+    let (part2, part1) = simulate (tail input) (parseInput (head input)) 0
+    print part1
+    print (sum part2)
